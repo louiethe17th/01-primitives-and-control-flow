65e905b85121d7a8b906e009309837a670c9501b
Tyler Pearson <tylerthewonderfull@gmail.com>
2018-07-14 15:40:56 -0700

    bug fixes

--- src/FlipNCoins.java
+++ src/FlipNCoins.java
@@ -2,7 +2,7 @@ import java.lang.Math;
 
 public class FlipNCoins {
     public static void main(String[] args) {
-        flipNHeads(10);
+        flipNHeads(4);
     }
 
     public static void flipNHeads(int n) {
